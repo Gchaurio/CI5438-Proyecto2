96f29e0d115c03ed9c7654672ad0f51e51804bc6
Gchaurio <17-10126@usb.ve>
2023-11-26 01:45:57 -0400

    Se agregaron comentarios a los codigos, se agrego el informe y se agregaron las graficas faltantes utilizadas

--- neuron.py
+++ neuron.py
@@ -9,18 +9,23 @@ class Neuron(object):
         self.values = None
         self.activation_value = None
 
+    
+    # Calcula valores 
     def calculate_values(self):
         
         return np.dot(self.values, self.weights)
 
+    # Funcion logistica
     def activation_function(self, z):
 
         return 1 / (1 + np.exp(-z))
     
+    # Derivada de funcion logistica
     def activation_function_derivate(self):
 
         return self.get_activation_value()*(1-self.get_activation_value())
     
+    # Retorna el valor de activacion de la neurona
     def get_activation_value(self):
 
         self.activation_value = self.activation_function(self.calculate_values())
